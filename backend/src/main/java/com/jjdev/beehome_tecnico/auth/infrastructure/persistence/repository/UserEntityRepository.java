18080a3619d73e6e7ca35dfa78605e82a66cddbd
Jailton Júnior <jailtonjr99@hotmail.com.br>
2026-07-20 20:45:26 -0300

    included new field so we can get logged user

--- backend/src/main/java/com/jjdev/beehome_tecnico/auth/infrastructure/persistence/repository/UserEntityRepository.java
+++ backend/src/main/java/com/jjdev/beehome_tecnico/auth/infrastructure/persistence/repository/UserEntityRepository.java
@@ -1,9 +1,12 @@
 package com.jjdev.beehome_tecnico.auth.infrastructure.persistence.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jjdev.beehome_tecnico.auth.infrastructure.persistence.entity.UserEntity;
 
 public interface UserEntityRepository extends JpaRepository<UserEntity, UUID> {
 	boolean existsByEmail(String email);
+
+	Optional<UserEntity> findByEmail(String email);
 }
