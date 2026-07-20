5f02f70ac1b916b4beefd294a4721d6b11f93c21
Jailton Júnior <jailtonjr99@hotmail.com.br>
2026-07-20 18:45:20 -0300

    initial backend commit

---
+++ backend/src/test/java/com/jjdev/beehome_tecnico/BeehomeTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjdev.beehome_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeehomeTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
