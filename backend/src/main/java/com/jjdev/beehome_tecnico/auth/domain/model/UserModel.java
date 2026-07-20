296ab832bea4d83c1d9f587285e2ffabaa3431f8
Jailton Júnior <jailtonjr99@hotmail.com.br>
2026-07-20 20:45:51 -0300

    adjusted user model so we don't need to inform all fields of edition

--- backend/src/main/java/com/jjdev/beehome_tecnico/auth/domain/model/UserModel.java
+++ backend/src/main/java/com/jjdev/beehome_tecnico/auth/domain/model/UserModel.java
@@ -2,6 +2,7 @@ package com.jjdev.beehome_tecnico.auth.domain.model;
 
 import com.jjdev.beehome_tecnico.shared.domain.model.BaseModel;
 
+import io.micrometer.common.util.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,9 +29,9 @@ public class UserModel extends BaseModel {
 	public UserModel update(String username, String email, String hashedPassword) {
 		return UserModel.builder()
 				.id(getId())
-				.username(username)
-				.email(email)
-				.password(hashedPassword)
+				.username(StringUtils.isNotBlank(username) ? username : getUsername())
+				.email(StringUtils.isNotBlank(email) ? email : getEmail())
+				.password(StringUtils.isNotBlank(hashedPassword) ? hashedPassword : getPassword())
 				.createdAt(getCreatedAt())
 				.updatedAt(getUpdatedAt())
 				.build();
