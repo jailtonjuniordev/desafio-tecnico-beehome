49c897b8e39873a1b41b83d22aa3e053d458b921
Jailton Júnior <jailtonjr99@hotmail.com.br>
2026-07-20 21:01:36 -0300

    included new methods in user service to be used inside code

--- backend/src/main/java/com/jjdev/beehome_tecnico/auth/application/service/UserService.java
+++ backend/src/main/java/com/jjdev/beehome_tecnico/auth/application/service/UserService.java
@@ -1,5 +1,8 @@
 package com.jjdev.beehome_tecnico.auth.application.service;
 
+import java.util.UUID;
+
+import com.jjdev.beehome_tecnico.auth.domain.model.UserModel;
 import com.jjdev.beehome_tecnico.auth.rest.dto.UserCreateRequestDTO;
 import com.jjdev.beehome_tecnico.auth.rest.dto.UserResponseDTO;
 import com.jjdev.beehome_tecnico.auth.rest.dto.UserUpdateRequestDTO;
@@ -11,4 +14,8 @@ public interface UserService {
 	UserResponseDTO getLogged();
 
 	UserResponseDTO update(UserUpdateRequestDTO request);
+
+	UserModel getUserByIdSystem(UUID id);
+
+	UserModel getUserByEmailSystem(String email);
 }
